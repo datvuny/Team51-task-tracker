ac1441da8a9ef6bd782b58520d8c5e22bcef4bef
Dat Vu <datqvu295@gmail.com>
2022-07-08 16:11:44 -0400

    second commit

--- client/components/EditTodo.js
+++ client/components/EditTodo.js
@@ -1,18 +1,94 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import {deleteTodo} from '../store/todos'
+import React from "react";
+import { connect } from "react-redux";
+import { deleteTodos } from "../store/todos";
+import {Link} from 'react-router-dom'
+import {fetchTodo} from '../store/todo'
 
 class EditTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskName: '',
+      assignee: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    console.log('#$#$$#$$#$$', this.props.taskName)
+  }
 
-    render(){
-        return(
-            <button>DELETE</button>
-        )
+  componentDidMount() {
+    const id = this.props.match.params.id
+    this.props.fetchTodo(id)
+  }
+
+  componentDidUpdate(prevProps){
+    // console.log(prevProps)
+    if(prevProps.taskName !== this.props.taskName){
+      this.setState({ taskName: this.props.todo.taskName });
+    }
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    this.props.createTodo({ ...this.state });
+  }
+
+  render() {
+    const { assignee, taskName } = this.state;
+    const { handleSubmit } = this;
+    const id = this.props.match.params.id;
+
+    return (
+      <div>
+        <form id="todo-form" onSubmit={handleSubmit}>
+          <label htmlFor="taskName">Task Name:</label>
+          <input
+            name="taskName"
+            onChange={this.handleChange}
+            value={taskName}
+          />
+
+          <label htmlFor="assignee">Assign To:</label>
+          <input
+            name="assignee"
+            onChange={this.handleChange}
+            value={assignee}
+          />
+
+          <button type="submit">Submit</button>
+          <button
+          onClick={() => {
+            this.props.deleteTodos(id);
+            this.props.history.push("/");
+          }}
+        >
+          DELETE
+          </button>
+          <Link to="/">Cancel</Link>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        todo: state.todo
     }
 }
 
-// const mapStateToProps = (state) => ({})
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteTodos: (id) => dispatch(deleteTodos(id)),
+    fetchTodo: (id) => dispatch(fetchTodo(id))
+  };
+};
 
-const mapDispatchToProps = (dispatch) => ({})
-
-export default connect(null, mapDispatchToProps)(EditTodo)
+export default connect(null, mapDispatchToProps)(EditTodo);
--- client/store/todo.js
+++ client/store/todo.js
@@ -1,4 +1,20 @@
+import axios from 'axios'
+
 const SET_TODO = 'SET_TODO';
+
+const setTodo = (todo) => {
+  return {
+    type: SET_TODO,
+    todo
+  }
+}
+
+export const fetchTodo = (id) => {
+  return async (dispatch) => {
+    const {data:todo} = await axios.get(`/api/todos/${id}`)
+    dispatch(setTodo(todo))
+  }
+}
 
 export default (state = {}, action) => {
   switch (action.type) {
